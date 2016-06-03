f8db9f68a5253db3daaec7156b9a952a94321c65
Thomas Lewis <thomasalewis1028@gmail.com>
2016-06-02 22:14:06 -0500

    Removed getShortDateTime()

--- haggisBot.js
+++ haggisBot.js
@@ -414,22 +414,6 @@ function getDateTime(){
     return hour + ":" + min + ":" + sec;
 }
 
-function getShortDateTime(){
-    var date = new Date();
-
-    var hour = date.getHours();
-    hour = (hour < 10 ? "0" : "") + hour;
-
-    var min  = date.getMinutes();
-    min = (min < 10 ? "0" : "") + min;
-
-    var sec  = date.getSeconds();
-    sec = (sec < 10 ? "0" : "") + sec;
-
-
-    return hour+min+sec;
-}
-
 //##ROLL DICE SHADOWRUN###
 function rollDiceSR(diceNum){
 	var results = "```\nResults: ";
